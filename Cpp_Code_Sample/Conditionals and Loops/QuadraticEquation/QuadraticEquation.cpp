9af89c303f205455e6741da2e5abd18352a9dda7
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2020-12-14 14:22:09 +0700

    Solve QE

--- Cpp_Code_Sample/Conditionals and Loops/QuadraticEquation/QuadraticEquation.cpp
+++ Cpp_Code_Sample/Conditionals and Loops/QuadraticEquation/QuadraticEquation.cpp
@@ -12,33 +12,31 @@ int main()
     GetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
     memcpy(consoleFont.FaceName, L"consolas", sizeof(L"consolas"));
     SetCurrentConsoleFontEx(hdlConsole, FALSE, &consoleFont);
-
+    system("cls");
     //Chạy chương trình đến khi người dùng ngưng sử dụng
-    while(1)
+    while (1)
     {
         //Nhập dữ liệu chương trình
         std::wcout << L"Giải Phương Trình Bậc Hai!" << std::endl;
-        std::wcout << L"ax\u00B2 + bx + c = 0" << std::endl;;
+        std::wcout << L"ax\u00B2 + bx + c = 0" << std::endl;
+        ;
         float a = Input_Value('a');
         float b = Input_Value('b');
         float c = Input_Value('c');
-        Print_Equation(a,b,c);//Hàm dùng để in ra màn hình phương trình đã nhập dữ liệu
-
+        Print_Equation(a, b, c); //Hàm dùng để in ra màn hình phương trình đã nhập dữ liệu
 
         //Giải chương trình
-        Result_QE x1,x2;
-        Solve_QuadraticEquation(a,b,c,&x1,&x2);
-        Print_Result_QE(x1,x2);
-
+        Result_QE x1, x2;
+        Solve_QuadraticEquation(a, b, c, &x1, &x2);
+        Print_Result_QE(x1, x2);
 
         //Lựa chọn thoát hoặc tiếp tục chương trình
         std::wcout << L"Ấn phím ESC để thoát chương trình...";
         getch();
-        if(GetKeyState(VK_ESCAPE) & 0x8000)
+        if (GetKeyState(VK_ESCAPE) & 0x8000)
         {
             return 0;
         }
         system("cls");
     }
-
 }
