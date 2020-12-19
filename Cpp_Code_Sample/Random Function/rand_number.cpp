da34209fcdfc8d089b66d849aae4432bc267dcf8
Lê An Nguyễn <nguyenlean2701@hotmail.com>
2020-12-20 02:27:55 +0700

    ok

---
+++ Cpp_Code_Sample/Random Function/rand_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(0));
+    int min, max;
+    min = 0;
+    max = 99;
+    for (int i = 0; i < 10; i++)
+    {
+        int rand_number = min + (rand() % (max - min + 1));
+        cout << rand_number << endl;
+    }
+    return 0;
+}
